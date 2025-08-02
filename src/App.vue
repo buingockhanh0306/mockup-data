7c7b77927c82bef45b97feca58516e953d33e89c
buingockhanh0306 <buikhanhi2k@gmail.com>
2025-08-03 02:28:10 +0700

    update copy

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,8 @@
               <BButton
                 size="sm"
                 variant="outline-primary"
-                :disabled="!targetCode.value"
+                :disabled="!targetCode"
+                @click="handleCopy"
                 >Copy</BButton
               >
             </template>
@@ -79,12 +80,10 @@ const getCode = () => {
     const obj = {};
 
     for (const [key, value] of Object.entries(jsonOriginal)) {
-      if (typeof value === "string") {
-        obj[key] = value.replace(/(\d+)?$/, "") + "_" + i;
-      } else if (typeof value === "number") {
-        obj[key] = value + i;
-      } else {
-        obj[key] = value;
+      if (key === primaryKey.value) {
+        obj[key] = typeof value === "number" ? value + 1 : `${value}_${i}`;
+      } else if (typeof value === "string") {
+        obj[key] = `${value}_${i}`;
       }
     }
 
@@ -93,12 +92,17 @@ const getCode = () => {
 
   targetCode.value = JSON.stringify(pushData, null, 2);
 };
+
+const handleCopy = async () => {
+  await navigator.clipboard.writeText(targetCode.value);
+};
 </script>
 
 <style scoped>
 body {
   font-family: "Poppins", sans-serif;
   margin: 0;
+  height: 100vh;
   padding: 2rem;
   background: linear-gradient(to right, #eef2f3, #ffffff);
   color: #333;
